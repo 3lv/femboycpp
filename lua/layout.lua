b22b7350fe938d510be5cda68aff674d1054a74d
3lv <cristian.vlad.vladutu@gmail.com>
2021-04-27 23:25:08 +0300

    first commit

---
+++ lua/layout.lua
@@ -0,0 +1,40 @@
+local function is_open(filename)
+	local winnr = vim.fn.bufwinnr(filename)
+	if winnr == -1 then
+		return false
+	end
+	return true
+end
+
+local function close_all(filename)
+	local winid = vim.fn.bufwinid(filename)
+	while winid ~= -1 do
+		vim.api.nvim_win_close(winid, true)
+		winid = vim.fn.bufwinid(filename)
+	end
+end
+
+local function toggle_inout()
+	local fullfilename = vim.fn.expand('%')
+	local filename = vim.fn.expand('%:r') --filename_without_extension
+	local f1 = filename .. '.in'
+	local f2 = filename .. '.out'
+	if is_open(f1) and is_open(f2) then
+		close_all(f1)
+		close_all(f2)
+	elseif not is_open(f1) and not is_open(f2) then
+		local winnr = vim.fn.bufwinnr(fullfilename)
+		vim.cmd[[bo 40vs %:r.in]]
+		vim.cmd[[bel sp %:r.out]]
+		vim.cmd(winnr .. "wincmd w")
+	elseif is_open(f1) then
+		close_all(f1)
+	elseif is_open(f2) then
+		close_all(f2)
+	end
+end
+
+local functions = {}
+functions.toggle_inout = toggle_inout
+
+return functions
