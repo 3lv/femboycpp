335b19e503e347734ef7f76789c0eb7fe58746d6
3lv <cristian.vlad.vladutu@gmail.com>
2021-04-28 13:18:56 +0300

    test1

--- lua/femboycpp.lua
+++ lua/femboycpp.lua
@@ -1,3 +1,12 @@
+local function get_project_name()
+	buflist = vim.api.nvim_list_bufs()
+	for buf in buflist do
+		if nvim_buf_is_loaded(buf) then
+			return buf
+		end
+	end
+end
+
 local function is_open(filename)
 	local winnr = vim.fn.bufwinnr(filename)
 	if winnr == -1 then
@@ -14,6 +23,7 @@ local function close_all(filename)
 	end
 end
 
+
 local function toggle_inout()
 	local fullfilename = vim.fn.expand('%')
 	local filename = vim.fn.expand('%:r') --filename_without_extension
@@ -24,8 +34,9 @@ local function toggle_inout()
 		close_all(f2)
 	elseif not is_open(f1) and not is_open(f2) then
 		local winnr = vim.fn.bufwinnr(fullfilename)
-		vim.cmd[[bo 40vs %:r.in|set nu nornu wfw wfh]]
-		vim.cmd[[bel sp %:r.out|set nu nornu wfw wfh]]
+		local opts = '!setlocal nu nornu wfw wfh'
+		vim.cmd('bo ' .. '40' .. 'vs ' .. f1 .. opts)
+		vim.cmd('bel sp' .. f2 .. opts)
 		vim.cmd(winnr .. "wincmd w")
 	elseif is_open(f1) then
 		close_all(f1)
